05ada793ac387931e88a72a02c2120d0b79d1c76
komikat <akshitdotkr@gmail.com>
2024-11-18 22:46:21 +0530

    dataset

--- src/graph/highest_degree.py
+++ src/graph/highest_degree.py
@@ -1,29 +1,38 @@
-import networkx as nx
 import random
+
+import networkx as nx
 
 
 def hop_to_highest_degree_node(G):
     node = random.choice(list(G.nodes))
     visited = {node}
-    
+    full = []
+
     while True:
         neighbours = list(G.successors(node)) + list(G.predecessors(node))
         neighbours = [n for n in neighbours if n not in visited]
-        
-        if not neighbours or len(visited) > 5: # restrict to 5 hops
+
+        if not neighbours or len(visited) > 5:  # restrict to 5 hops
             break
-            
+
         degrees = [(n, G.degree(n)) for n in neighbours]
         degrees.sort(key=lambda x: x[1], reverse=True)
         new_node = degrees[0][0]
-        
-        label = G[node][new_node]["relation"] if new_node in G[node] else G[new_node][node]["relation"]
-        print(f"{node} - {label} - {new_node}")
-        
+
+        label = (
+            G[node][new_node]["relation"]
+            if new_node in G[node]
+            else G[new_node][node]["relation"]
+        )
+        full.append((node, label, new_node))
+
         visited.add(new_node)
         node = new_node
 
-if __name__=="__main__":
+    return full
+
+
+if __name__ == "__main__":
     G = nx.read_graphml("../../dataset/MetaQA/movies.graphml")
     print(f"Graph loaded with {len(G.nodes)} nodes!")
-    hop_to_highest_degree_node(G)
+    print(hop_to_highest_degree_node(G))
--- src/graph/init.py
+++ src/graph/init.py
@@ -1,6 +1,10 @@
 import networkx as nx
+import json
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 with open("../../dataset/MetaQA/kb.txt") as f:
     full = f.read().split("\n")
@@ -11,8 +15,12 @@ G = nx.DiGraph()
 for line in tqdm(full):
     if len(line.split("|")) == 3:
         movie, rel, ent = line.split("|")
-        G.add_node(movie, type="movie")
-        G.add_node(ent)
+        if not G.has_node(movie):
+            embedding = model.encode(movie).tolist()
+            G.add_node(movie, type="movie", embedding=json.dumps(embedding))
+        if not G.has_node(ent):
+            embedding = model.encode(ent).tolist()
+            G.add_node(ent, embedding=json.dumps(embedding))
         G.add_edge(movie, ent, relation=rel)
 
 nx.write_graphml(G, "../../dataset/MetaQA/movies.graphml")
